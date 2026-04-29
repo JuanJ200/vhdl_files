28afd9e3dac94bc8bd9a1bea081b41ab880960e6
JuanJ200 <juanjyacumalr@gmail.com>
2026-04-29 18:30:03 -0500

    Refactor pkg_mem.vhd to include constants and component

--- Microproyecto2/Practica/pkg_mem.vhd
+++ Microproyecto2/Practica/pkg_mem.vhd
@@ -2,4 +2,16 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
 package pkg_mem is
+    constant DW : integer := 8;
+    constant AW : integer := 2;
+    constant DEPTH : integer := 4;
+    type fsm_t is (INI, READ_ROM, WRITE_RAM, READ_RAM, SHOW);
+
+    component rom_sync
+        port(
+            clk      : in  std_logic;
+            addr     : in  std_logic_vector(AW-1 downto 0);
+            data_out : out std_logic_vector(DW-1 downto 0)
+        );
+    end component;
 end package;
