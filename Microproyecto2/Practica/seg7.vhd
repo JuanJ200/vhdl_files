0287c9c387b871bb76872ffb94642a8c154729dc
JuanJ200 <juanjyacumalr@gmail.com>
2026-04-29 18:26:35 -0500

    Update seg7.vhd

--- Microproyecto2/Practica/seg7.vhd
+++ Microproyecto2/Practica/seg7.vhd
@@ -10,4 +10,20 @@ end;
 
 architecture rtl of seg7 is
 begin
+    process(digit)
+    begin
+        case digit is
+            when "0000" => seg <= "0000001";
+            when "0001" => seg <= "1001111";
+            when "0010" => seg <= "0010010";
+            when "0011" => seg <= "0000110";
+            when "0100" => seg <= "1001100";
+            when "0101" => seg <= "0100100";
+            when "0110" => seg <= "0100000";
+            when "0111" => seg <= "0001111";
+            when "1000" => seg <= "0000000";
+            when "1001" => seg <= "0000100";
+            when others => seg <= "1111111";
+        end case;
+    end process;
 end rtl;
