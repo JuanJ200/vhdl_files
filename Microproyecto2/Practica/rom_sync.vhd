c55cb3517efa065a6094931df1f0228e66e9ac63
JuanJ200 <juanjyacumalr@gmail.com>
2026-04-29 18:29:25 -0500

    Define ROM array and assign data_out
    Added ROM initialization and output assignment.

--- Microproyecto2/Practica/rom_sync.vhd
+++ Microproyecto2/Practica/rom_sync.vhd
@@ -10,5 +10,8 @@ entity rom_sync is
 end;
 
 architecture rtl of rom_sync is
+    type rom_array is array (0 to 3) of std_logic_vector(7 downto 0);
+    constant ROM : rom_array := ("00010010", "11001010", "00000111", "10101111");
 begin
+    data_out <= ROM(0); -- Línea temporal para probar pines
 end rtl;
