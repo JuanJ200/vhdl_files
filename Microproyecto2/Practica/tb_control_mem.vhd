4fb8cd8b54cdf0e9bb6489d0adc86f70002f72d5
JuanJ200 <juanjyacumalr@gmail.com>
2026-04-30 23:51:23 -0500

    Refactor testbench for control memory entity

--- Microproyecto2/Practica/tb_control_mem.vhd
+++ Microproyecto2/Practica/tb_control_mem.vhd
@@ -1,28 +1,42 @@
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 
-entity tb_control_mem is
+entity tb_control_memorias is
 end;
 
-architecture sim of tb_control_mem is
+architecture sim of tb_control_memorias is
+
     signal clk : std_logic := '0';
     signal rst : std_logic := '0';
+
     signal re, we : std_logic;
     signal addr : std_logic_vector(1 downto 0);
     signal data_in, data_out : std_logic_vector(7 downto 0);
+
     signal su, sd, sc : std_logic_vector(6 downto 0);
+
 begin
+
     DUT: entity work.control_memorias
         port map(clk, rst, re, we, addr, data_in, data_out, su, sd, sc);
 
-    clk_process: process begin
-        clk <= '0'; wait for 10 ms;
-        clk <= '1'; wait for 10 ms;
+    clk_process: process
+    begin
+        while true loop
+            clk <= '0'; wait for 10 ms;
+            clk <= '1'; wait for 10 ms;
+        end loop;
     end process;
 
-    stim: process begin
-        rst <= '0'; wait for 30 ms;
-        rst <= '1'; wait for 500 ms;
+    stim: process
+    begin
+        rst <= '0';
+        wait for 30 ms;
+        rst <= '1';
+
+        wait for 500 ms;
+
         wait;
     end process;
-end architecture;
+
+end;
