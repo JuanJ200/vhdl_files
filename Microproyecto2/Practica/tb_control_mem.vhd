5675245f6b129d5d55626517d0cdfa9774ca9d4c
JuanJ200 <juanjyacumalr@gmail.com>
2026-05-02 00:24:55 -0500

    Update tb_control_mem.vhd

--- Microproyecto2/Practica/tb_control_mem.vhd
+++ Microproyecto2/Practica/tb_control_mem.vhd
@@ -1,4 +1,5 @@
-library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
 entity tb_control_mem is end entity;
 architecture sim of tb_control_mem is
     signal clk : std_logic := '0'; signal rst : std_logic := '0';
