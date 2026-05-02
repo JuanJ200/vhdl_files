00d0c992023eb8ce2058a256b5aa005155d70a8b
JuanJ200 <juanjyacumalr@gmail.com>
2026-05-02 00:29:36 -0500

    Update seg7.vhd

--- Microproyecto2/Practica/seg7.vhd
+++ Microproyecto2/Practica/seg7.vhd
@@ -1,4 +1,5 @@
-library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+library IEEE; 
+use IEEE.STD_LOGIC_1164.ALL;
 entity seg7 is
     port(digit: in std_logic_vector(3 downto 0); seg: out std_logic_vector(6 downto 0));
 end entity;
