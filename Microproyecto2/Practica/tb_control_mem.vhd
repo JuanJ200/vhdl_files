bdf3ecdd74266f866c83946a2c802b65452ed254
JuanJ200 <juanjyacumalr@gmail.com>
2026-05-02 00:21:23 -0500

    Refactor tb_control_mem VHDL testbench
    Removed unused signals and adjusted entity mapping.

--- Microproyecto2/Practica/tb_control_mem.vhd
+++ Microproyecto2/Practica/tb_control_mem.vhd
@@ -1,42 +1,19 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-
-entity tb_control_memorias is
-end;
-
-architecture sim of tb_control_memorias is
-
-    signal clk : std_logic := '0';
-    signal rst : std_logic := '0';
-
-    signal re, we : std_logic;
-    signal addr : std_logic_vector(1 downto 0);
-    signal data_in, data_out : std_logic_vector(7 downto 0);
-
+library IEEE; use IEEE.STD_LOGIC_1164.ALL;
+entity tb_control_mem is end entity;
+architecture sim of tb_control_mem is
+    signal clk : std_logic := '0'; signal rst : std_logic := '0';
     signal su, sd, sc : std_logic_vector(6 downto 0);
-
 begin
-
-    DUT: entity work.control_memorias
-        port map(clk, rst, re, we, addr, data_in, data_out, su, sd, sc);
-
-    clk_process: process
-    begin
+    DUT: entity work.control_memorias port map(clk, rst, su, sd, sc);
+    clk_process: process begin
         while true loop
             clk <= '0'; wait for 10 ms;
             clk <= '1'; wait for 10 ms;
         end loop;
     end process;
-
-    stim: process
-    begin
-        rst <= '0';
-        wait for 30 ms;
-        rst <= '1';
-
-        wait for 500 ms;
-
-        wait;
+    stim: process begin
+        rst <= '0'; wait for 30 ms;
+        rst <= '1'; wait for 500 ms;
+        assert false report "Simulacion OK" severity note; wait;
     end process;
-
-end;
+end architecture;
