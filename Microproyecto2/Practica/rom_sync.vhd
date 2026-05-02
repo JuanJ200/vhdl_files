c049cfbde1e1338c8ab06ddf62961cafed0f2be0
JuanJ200 <juanjyacumalr@gmail.com>
2026-05-02 00:25:43 -0500

    Update rom_sync.vhd

--- Microproyecto2/Practica/rom_sync.vhd
+++ Microproyecto2/Practica/rom_sync.vhd
@@ -1,34 +1,15 @@
-library IEEE;
-use IEEE.STD_LOGIC_1164.ALL;
-use IEEE.NUMERIC_STD.ALL;
-use work.pkg_mem.all;
-
+library IEEE; 
+use IEEE.STD_LOGIC_1164.ALL; use IEEE.NUMERIC_STD.ALL; use work.pkg_mem.all;
 entity rom_sync is
-    port(
-        clk      : in  std_logic;
-        addr     : in  std_logic_vector(AW-1 downto 0);
-        data_out : out std_logic_vector(DW-1 downto 0)
-    );
-end;
-
+    port(clk: in std_logic; addr: in std_logic_vector(AW-1 downto 0); data_out: out std_logic_vector(DW-1 downto 0));
+end entity;
 architecture rtl of rom_sync is
-
     type rom_array is array (0 to DEPTH-1) of std_logic_vector(DW-1 downto 0);
-
-    constant ROM : rom_array := (
-        "00010010", -- 18
-        "11001010", -- 202
-        "00000111", -- 7
-        "10101111"  -- 175
-    );
-
+    constant ROM : rom_array := ("00010010", "11001010", "00000111", "10101111");
 begin
-
-    process(clk)
-    begin
+    process(clk) begin
         if rising_edge(clk) then
             data_out <= ROM(to_integer(unsigned(addr)));
         end if;
     end process;
-
-end rtl;
+end architecture;
