6c75c058025a34b9ecc7ea2c5aa1478167b3d147
JuanJ200 <juanjyacumalr@gmail.com>
2026-05-02 00:26:25 -0500

    Update pkg_mem.vhd
    Added ROM and RAM component declarations to pkg_mem.

--- Microproyecto2/Practica/pkg_mem.vhd
+++ Microproyecto2/Practica/pkg_mem.vhd
@@ -1,12 +1,16 @@
-library IEEE;
+library IEEE; 
 use IEEE.STD_LOGIC_1164.ALL;
 
 package pkg_mem is
-
-    constant DW : integer := 8;
-    constant AW : integer := 2;
-    constant DEPTH : integer := 4;
-
+    
+    constant DW : integer := 8; constant AW : integer := 2; constant DEPTH : integer := 4;
     type fsm_t is (INI, READ_ROM, WRITE_RAM, READ_RAM, SHOW);
 
+    component rom_sync is
+        port(clk: in std_logic; addr: in std_logic_vector(AW-1 downto 0); data_out: out std_logic_vector(DW-1 downto 0));
+    end component;
+        
+    component ram_sincrona is
+        port(clk, wr_en, rd_en: in std_logic; addr: in std_logic_vector(AW-1 downto 0); data_in: in std_logic_vector(DW-1 downto 0); data_out: out std_logic_vector(DW-1 downto 0));
+    end component;
 end package;
